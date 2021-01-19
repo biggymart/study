75f0fd62db5150f5b4a20a2120aff9296a6393d4
Choi Yeachan <biggymart@naver.com>
2021-01-19 10:03:43 +0900

    0119_1st

--- keras/keras55_split_def2.py
+++ keras/keras55_split_def2.py
@@ -1,30 +1,65 @@
 import numpy as np
-# def split_xy(Data, x_col, x_row, y_col, y_row)
-
-#문제2.
-dataset = np.array([[1,2,3,4,5,6,7,8,9,10], [11,12,13,14,15,16,17,18,19,20], [21,22,23,24,25,26,27,28,29,30]])
+dataset = np.array([[1,2,3,4,5,6,7,8,9,10],[11,12,13,14,15,16,17,18,19,20],[21,22,23,24,25,26,27,28,29,30]])
 dataset = np.transpose(dataset)
-print(dataset)
-print(dataset.shape)
-print(dataset[0])
 
-# def split_xy1(dataset, x_row, x_col, y_row, y_col):
-#     x_lst, y_lst = list(), list()
-#     for i in range(len(dataset)):
-#         x_row_end = i + x_row
-#         x_col_end = i + x_col
-#         y_row_end = i + y_row
-#         y_col_end = i + y_col
-#         # if y_end_number > len(dataset):
-#             # break
-#         tmp_x = dataset[i:x_row_end, i:x_col_end]
-#         tmp_y = dataset[i:y_row_end, i:y_col_end]
-#         x_lst.append(tmp_x)
-#         y_lst.append(tmp_y)
-#     return np.array(x_lst), np.array(y_lst)
-# x, y = split_xy1(dataset, 2, 2, 2 ,2)
-# print(x)
-# print(y)
+def split_xy3(dataset2, x_row, x_col, y_row, y_col):
+    x,y = list(), list()
+    for i in range(len(dataset)):
+        x_start_number = i
+        x_end_number = i + x_row
+        y_end_number = x_end_number + y_row -1
+        
+        if y_end_number > len(dataset):
+            break
+        tmp_x = dataset[i:x_end_number, : x_col]
+        tmp_y = dataset[x_end_number -1 : y_end_number, x_col :]
+        x.append(tmp_x)
+        y.append(tmp_y)
+    return np.array(x), np.array(y)
 
+x, y = split_xy3(dataset,3,2,4,1)
+print(x, '\n', y)
+print(x.shape)
+print(y.shape)
 
-#   return
+'''
+[[[ 1 11]
+  [ 2 12]
+  [ 3 13]]
+ [[ 2 12]
+  [ 3 13]
+  [ 4 14]]
+ [[ 3 13]
+  [ 4 14]
+  [ 5 15]]
+ [[ 4 14]
+  [ 5 15]
+  [ 6 16]]
+ [[ 5 15]
+  [ 6 16]
+  [ 7 17]]] 
+
+[[[23]
+  [24]
+  [25]
+  [26]]
+ [[24]
+  [25]
+  [26]
+  [27]]
+ [[25]
+  [26]
+  [27]
+  [28]]
+ [[26]
+  [27]
+  [28]
+  [29]]
+ [[27]
+  [28]
+  [29]
+  [30]]]
+  
+(5, 3, 2)
+(5, 4, 1)
+'''
