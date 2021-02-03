13184efb7ff7ecbc075de6235969731e9b18df25
Choi Yeachan <biggymart@naver.com>
2021-02-03 22:20:34 +0900

    0203_nighty_night

--- keras/keras01_OverallWorkflow.py
+++ keras/keras01_OverallWorkflow.py
@@ -1,11 +1,11 @@
 import numpy as np
 import tensorflow as tf
 
-#1. 데이터 준비
+#1. data
 x = np.array([1,2,3])
 y = np.array([1,2,3])
 
-#2. 모델 구성 (The task is hyper-parameter tunning)
+#2. model (The task is hyper-parameter tunning)
 from tensorflow.keras.models import Sequential # 신경망에서 한 단계씩 거치니까 순차적인 모델임
 from tensorflow.keras.layers import Dense # DNN 을 만들겠다, 가장 기본적인 모델
 
@@ -15,7 +15,7 @@ model.add(Dense(3, activation='linear')) # 활성화 함수
 model.add(Dense(4))
 model.add(Dense(1))
 
-#3. 컴파일, 훈련
+#3. compile and fit
 '''
 from tensorflow.keras.optimizers import Adam, SGD
 optimizer1 = Adam(learning_rate=0.1)
@@ -27,7 +27,7 @@ model.compile(loss='mse', optimizer=optimizer2)
 model.compile(loss='mse', optimizer='adam') # mean_squared_error
 model.fit(x, y, epochs=100, batch_size=1) # 휘트니스센터에서 훈련해라, epochs 훈련하는 회수, batch_size 한칸씩 짤라서 써라
 
-#4. 평가, 예측
+#4. evaluate and predict
 loss = model.evaluate(x, y, batch_size=1)
 print('Loss :', loss)
 
