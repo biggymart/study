108e71f8b9259ad9f87576e192b33d165b870246
Choi Yeachan <biggymart@naver.com>
2021-02-09 11:38:46 +0900

    plzzz

--- keras2/keras65_pipeline.py
+++ keras2/keras65_pipeline.py
@@ -7,19 +7,19 @@ import numpy as np
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Dense, Dropout, Input
 from tensorflow.keras.datasets import mnist
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 #1. data and preprocessing
-from tensorflow.keras.utils import to_categorical
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
-
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-
-from sklearn.model_selection import KFold
-kfold = KFold(n_splits=3, shuffle=True, random_state=66)
 
 x_train = x_train.reshape(60000, 28*28).astype('float32')
 x_test = x_test.reshape(10000, 28*28).astype('float32')
@@ -40,79 +40,53 @@ def build_model(drop=0.5, optimizer='adam'):
     return model
 
 def create_hyperparameters():
-    batches = [10, 20, 30, 40, 50]
+    batches = [10, 30, 50]
     optimizers = ['rmsprop', 'adam', 'adadelta']
-    dropout = [0.1, 0.2, 0.3]
-    return {"aaa__batch_size" : batches, "aaa__optimizer" : optimizers,
+    dropout = [0.2, 0.3]
+    return {"aaa__batch_size" : batches, 
+            "aaa__optimizer" : optimizers,
             "aaa__drop" : dropout}
 hyperparameters = create_hyperparameters()
-model2 = build_model()
 
 # allows Grid/RandomizedSearch to understand keras models
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 model2 = KerasClassifier(build_fn=build_model, verbose=1) 
 
-from sklearn.pipeline import Pipeline, make_pipeline
+scaler = MinMaxScaler()
 pipe = Pipeline([("scaler", scaler), ('aaa', model2)])
-pipe.get_params().keys()
 
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+kfold = KFold(n_splits=3, shuffle=True, random_state=66)
 search = RandomizedSearchCV(pipe, hyperparameters, cv=kfold)
 
-from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
 es = EarlyStopping(monitor='val_loss', patience=5)
 re = ReduceLROnPlateau(monitor='val_loss', patience=3)
 cp = ModelCheckpoint(filepath='C:/data/modelCheckpoint/k61_{epoch:02d}_{val_loss:.4f}.hdf5', monitor='val_loss', save_best_only=True, mode='auto')
 
 search.fit(x_train, y_train, aaa__epochs=100, aaa__validation_split=0.2, aaa__callbacks=[es, re, cp])
 
+
+print("best_params : ", search.best_params_)         
+print("best_estimator : ", search.best_estimator_)   
+print("best_score : ", search.best_score_)           
+
 acc = search.score(x_test, y_test)
-print("Final score: ", acc)
-print(search.best_params_)
-print(search.best_estimator_)
-print(search.best_score_)
-print(search.cv_results_)
+print("Score : ", acc)
 
-from sklearn.externals import joblib
-joblib.dump(search.best_estimator_, '../data/h5/k64.pkl', compress=1)
+search.best_estimator_.model.save('../data/h5/k65_save.h5')
 
-# Final score:  0.9860000014305115
-# {'optimizer': 'adam', 'drop': 0.1, 'batch_size': 40}
-# <tensorflow.python.keras.wrappers.scikit_learn.KerasClassifier object at 0x0000026F0D121E20>
-# 0.9804500142733256
-# {'mean_fit_time': array([ 80.78310458,  16.1492552 , 217.34447304, 278.15846189,
-#        218.27508656,  28.53572003,  19.36032772,  21.41552202,
-#         33.24328272,  23.03036928]), 'std_fit_time': array([4.97633911, 1.17580648, 0.99905162, 2.26659108, 1.00987607,
-#        1.97611169, 1.39250416, 2.90685582, 0.90435215, 1.30821381]), 'mean_score_time': array([2.35113835, 0.69550284, 1.06699347, 1.22564356, 1.06532892,
-#        1.09105794, 0.68022052, 1.28213072, 1.06567844, 1.24383648]), 'std_score_time': array([0.01710891, 0.00394651, 0.00665468, 0.0040017 , 0.01217912,
-#        0.06232058, 0.00772865, 0.05342676, 0.01181792, 0.01156329]), 'param_optimizer': masked_array(data=['adam', 'adam', 'adadelta', 'adadelta', 'adadelta',
-#                    'adam', 'adam', 'rmsprop', 'adam', 'rmsprop'],
-#              mask=[False, False, False, False, False, False, False, False,
-#                    False, False],
-#        fill_value='?',
-#             dtype=object), 'param_drop': masked_array(data=[0.3, 0.1, 0.2, 0.2, 0.3, 0.2, 0.3, 0.2, 0.3, 0.3],
-#              mask=[False, False, False, False, False, False, False, False,
-#                    False, False],
-#        fill_value='?',
-#             dtype=object), 'param_batch_size': masked_array(data=[10, 40, 30, 20, 30, 30, 40, 20, 30, 20],
-#              mask=[False, False, False, False, False, False, False, False,
-#                    False, False],
-#        fill_value='?',
-#             dtype=object), 'params': [{'optimizer': 'adam', 'drop': 0.3, 'batch_size': 10}, {'optimizer': 'adam', 'drop': 0.1, 'batch_size': 40}, {'optimizer': 'adadelta', 'drop': 0.2, 'batch_size': 30}, {'optimizer': 'adadelta', 'drop': 0.2, 'batch_size': 20}, {'optimizer': 'adadelta', 'drop': 0.3, 'batch_size': 30}, {'optimizer': 'adam', 'drop': 0.2, 'batch_size': 30}, {'optimizer': 'adam', 'drop': 0.3, 'batch_size': 40}, {'optimizer': 'rmsprop', 'drop': 0.2, 'batch_size': 20}, {'optimizer': 'adam', 'drop': 0.3, 'batch_size': 30}, {'optimizer': 'rmsprop', 'drop': 0.3, 'batch_size': 20}], 'split0_test_score': array([0.98150003, 0.98185003, 0.9149    , 0.92079997, 0.90555   ,
-#        0.9817    , 0.98220003, 0.97579998, 0.98154998, 0.97535002]), 'split1_test_score': array([0.97944999, 0.98000002, 0.90925002, 0.91415   , 0.89929998,
-#        0.9799    , 0.97955   , 0.97549999, 0.98035002, 0.97280002]), 'split2_test_score': array([0.97939998, 0.9795    , 0.91734999, 0.92025   , 0.90665001,
-#        0.97895002, 0.97764999, 0.9738    , 0.97850001, 0.97215003]), 'mean_test_score': array([0.98011667, 0.98045001, 0.91383334, 0.91839999, 0.90383333,
-#        0.98018334, 0.97980001, 0.97503332, 0.98013333, 0.97343336]), 'std_test_score': array([0.0009784 , 0.00101079, 0.00339173, 0.00301357, 0.00323687,
-#        0.00114041, 0.00186594, 0.00088065, 0.00125454, 0.00138102]), 'rank_test_score': array([ 4,  1,  9,  8, 10,  2,  5,  6,  3,  7])}
+# y_pred = search.predict(x_test)
+# r2 = r2_score(y_test, y_pred)
+# print("r2 : ", r2)
 
-# Traceback (most recent call last):
-#   File "c:\Study\keras2\keras64_save.py", line 66, in <module>
-#     search.save('../data/h5/k64.h5')
-# AttributeError: 'RandomizedSearchCV' object has no attribute 'save'
+# print("========model load========")
+# model3 = load_model('../data/h5/k64_save.h5')
+# print("best_score : ", model3.best_score_)           
 
-# Traceback (most recent call last):
-#   File "c:\Study\keras2\keras65_pipeline.py", line 77, in <module>
-#     save_model(search, filepath='../data/h5/k64.h5')
-#   File "C:\Users\ai\Anaconda3\lib\site-packages\tensorflow\python\keras\saving\save.py", line 121, in save_model
-#     if (not model._is_graph_network and  # pylint:disable=protected-access
-# AttributeError: 'RandomizedSearchCV' object has no attribute '_is_graph_network'
+import pickle   # 실패
+pickle.dump(search, open('../data/h5/k65.pickle.data', 'wb')) # wb : write
+print("== save complete ==")
+
+print("========pickle load========")
+# model3 = pickle.load(open('../data/h5/k64.pickle.data', 'rb'))
+# print("== load complete ==")
+# r2_2 = model3.score(x_test, y_test)
+# print("r2_2 : ", r2_2)
---
+++ keras2/keras66_ImageDataGenerator1.py
@@ -0,0 +1,62 @@
+import numpy as np
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# 이미지를 데이터로 전처리하는 것 (증폭, 변환)
+# OpenCV, loadimage 있는데 그 중 하나일 뿐
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    horizontal_flip=True,
+    vertical_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    rotation_range=5,
+    zoom_range=1.2,
+    shear_range=0.7,
+    fill_mode='nearest' # 최근접기법
+)
+# 현재 어떤 이미지도 로딩하지 않았고
+# 이런 훈련과 테스트 셋을 준비하겠다고 단순히 선언한 것일 뿐 
+# (이렇게 변환하겠다, 함수 설정과 비슷)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+# 훈련할 때는 충분히 많이 훈련하는 게 좋겠지만
+# 시험 잘 보려고 하는데 진짜 시험 문제를 증폭시키고 싶니?
+
+# flow 혹은 flow_from_directory (데이터로 만들어준다, 폴더 구조로 레이블해줄 수 있음)
+
+# train_generator
+# 이미지의 데이터화 (fit과 유사)
+xy_train = train_datagen.flow_from_directory(
+    '../data/image/brain/train', 
+    # flow의 경우 데이터를 넣어주면 됨 
+    # ad와 normal 폴더 안에 있는 80개 이미지 전체를 데이터화
+    target_size=(150, 150),
+    color_mode='grayscale',
+    classes=['ad', 'normal'],
+    # 임의의 사이즈 설정해줌 shape=(80, 150, 150, 1)
+    batch_size=5, 
+    # xy_train[0]을 print해보면 x가 5, y가 5 들어가 있는 걸 볼 수 있다 
+    # xy_train[0][0]은 x부분, xy_train[0][1]은 부분
+    # xy_train[0][0].shape은 (5, 150, 150, 1)
+    # xy_train[0][1].shape은 (5,)
+    # xy_train[0][1]은 0과 1로 구성됨
+    class_mode='binary'
+) # Found 160 images belonging to 2 classes.
+
+# test_generator
+xy_test = test_datagen.flow_from_directory(
+    '../data/image/brain/test',
+    target_size=(150, 150),
+    color_mode='grayscale',
+    classes=['ad', 'normal'],
+    batch_size=5,
+    class_mode='binary'
+) # Found 120 images belonging to 2 classes.
+
+# 생성이 되었는지 확인해보자
+print(xy_train)
+# <tensorflow.python.keras.preprocessing.image.DirectoryIterator object at 0x000001FA9BB18550>
+# sklearn.dataset도 이런 구조로 들어가 있지 (x, y가 함께 한 군데로 저장)
+
+print(xy_train[0])
+# 첫번째 batch
