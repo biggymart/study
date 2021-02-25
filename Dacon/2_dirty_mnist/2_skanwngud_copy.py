b5e97bebb017de26a1830a8b96a9121856e7966a
Choi Yeachan <biggymart@naver.com>
2021-02-25 18:16:25 +0900

    0225

---
+++ Dacon/2_dirty_mnist/2_skanwngud_copy.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+number = 5000
+
+for a in np.arange(0, 5):
+    Img = cv2.imread('c:/data/mnist/dirty_mnist_2nd/' + str(a).zfill(5) + '.png', cv2.IMREAD_GRAYSCALE)
+    #254보다 작고 0이아니면 0으로 만들어주기
+    img2 = np.where((Img <= 254) & (Img != 0), 0, Img)
+    # 이미지 팽창
+    img2 = cv2.dilate(img2, kernel=np.ones((2, 2), np.uint8), iterations=1)
+    # 블러 적용, 노이즈 제거
+    img2 = cv2.medianBlur(src=img2, ksize= 5)
+    # canny
+    # img2 = cv2.Canny(img2, 30, 70)
+
+    cv2.imwrite('c:/data/mnist/dirty_mnist_2nd/'+ str(a).zfill(5) + '.png', img2)
+
+print('done')
--- Dacon/2_dirty_mnist/3_clean_version.py
+++ Dacon/2_dirty_mnist/3_clean_version.py
@@ -1,4 +1,5 @@
 import os
+import imutils
 import cv2 as cv
 import pandas as pd
 import numpy as np
@@ -6,17 +7,40 @@ from matplotlib import pyplot as plt
 
 train_base_dir = 'C:/data/mnist/dirty_mnist_2nd/'
 test_base_dir = 'C:/data/mnist/test_dirty_mnist_2nd/'
-
 train_file_lst = os.listdir(train_base_dir)
 test_file_lst = os.listdir(test_base_dir)
 
-img = cv.imread(train_base_dir + train_file_lst[0], flags=cv.IMREAD_GRAYSCALE)
-train_label = pd.read_csv('C:/data/mnist/dirty_mnist_2nd_answer.csv', index_col='index')
+img = cv.imread(train_base_dir + train_file_lst[0])
+img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('original', img)
+
+# blur = cv.medianBlur(img, 5)
+# blur = cv.blur(img, (3,3))
+# blur = cv.GaussianBlur(img,(5,5),0)
+blur = cv.bilateralFilter(img,9,75,75)
+
+cv.imshow('blur', blur)
+# train_label = pd.read_csv('C:/data/mnist/dirty_mnist_2nd_answer.csv', index_col='index')
 
 kernel = np.ones((3, 3), np.uint8)
-erosion = cv.erode(img, kernel, iterations = 1)
+erosion = cv.erode(img,kernel,iterations = 1)
+cv.imshow('erosion', erosion)
+
 ret, thresh = cv.threshold(erosion, 100, 255, cv.THRESH_BINARY)
+cv.imshow('thresh', thresh)
+
+# cv.imwrite()
+##################################################################################################
+
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# cv.RETR_TREE, cv.RETR_LIST
+'''
+for i, contour in enumerate(contours):
+     x, y, w, h = cv.boundingRect(contour)
+     cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+'''
+cv.drawContours(thresh, contours, -1, (0,255,0), 3)
+print("Number of contours :", str(len(contours)))
 
 cv.imshow('Contours', thresh)
 cv.waitKey(0)
